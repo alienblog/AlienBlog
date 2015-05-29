4224ab847b1bc375d2a2434ec7b8e4e26852f78c
alien <alienblog@outlook.com>
2015-05-29 09:47:06 +0800

    add services

---
+++ AlienBlog.Application/PostAppService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Application.Services;
+using Abp.Domain.Repositories;
+
+namespace AlienBlog
+{
+	public class PostAppService : ApplicationService, IPostAppService
+	{
+		private readonly IRepository<Post> _postRepository;
+
+		public PostAppService(IRepository<Post> postRepository)
+		{
+			_postRepository = postRepository;
+		}
+	}
+}
--- AlienBlog.EntityFramework/EntityFramework/AlienBlogDbContext.cs
+++ AlienBlog.EntityFramework/EntityFramework/AlienBlogDbContext.cs
@@ -1,33 +1,35 @@
-﻿using Abp.EntityFramework;
+﻿using System.Data.Entity;
+using Abp.EntityFramework;
 
 namespace AlienBlog.EntityFramework
 {
-    public class AlienBlogDbContext : AbpDbContext
-    {
-        //TODO: Define an IDbSet for each Entity...
+	public class AlienBlogDbContext : AbpDbContext
+	{
+		//TODO: Define an IDbSet for each Entity...
 
-        //Example:
-        //public virtual IDbSet<User> Users { get; set; }
+		//Example:
+		//public virtual IDbSet<User> Users { get; set; }
+		public virtual IDbSet<Post> Posts { get; set; } 
 
-        /* NOTE: 
-         *   Setting "Default" to base class helps us when working migration commands on Package Manager Console.
-         *   But it may cause problems when working Migrate.exe of EF. If you will apply migrations on command line, do not
-         *   pass connection string name to base classes. ABP works either way.
-         */
-        public AlienBlogDbContext()
-            : base("Default")
-        {
+		/* NOTE: 
+		 *   Setting "Default" to base class helps us when working migration commands on Package Manager Console.
+		 *   But it may cause problems when working Migrate.exe of EF. If you will apply migrations on command line, do not
+		 *   pass connection string name to base classes. ABP works either way.
+		 */
+		public AlienBlogDbContext()
+			: base("Default")
+		{
 
-        }
+		}
 
-        /* NOTE:
-         *   This constructor is used by ABP to pass connection string defined in AlienBlogDataModule.PreInitialize.
-         *   Notice that, actually you will not directly create an instance of AlienBlogDbContext since ABP automatically handles it.
-         */
-        public AlienBlogDbContext(string nameOrConnectionString)
-            : base(nameOrConnectionString)
-        {
+		/* NOTE:
+		 *   This constructor is used by ABP to pass connection string defined in AlienBlogDataModule.PreInitialize.
+		 *   Notice that, actually you will not directly create an instance of AlienBlogDbContext since ABP automatically handles it.
+		 */
+		public AlienBlogDbContext(string nameOrConnectionString)
+			: base(nameOrConnectionString)
+		{
 
-        }
-    }
+		}
+	}
 }
